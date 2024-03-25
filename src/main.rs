9ab5413c2fc81837767f2693ac35e39a50a972a8
WUXIN <myeagleandsnake@gmail.com>
2024-03-25 15:27:29 +0800

    feat: aysnc

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize; // 导入 Deserialize 特性
-
+use tokio::process::Command;
 #[get("/read/later")]
 async fn read_later(params: web::Query<Params>) -> impl Responder {
     // 尝试从环境变量中获取名为SAVE_DIR的值
-    // let save_dir = match std::env::var("SAVE_DIR") {
-    //     Ok(val) => val,
-    //     Err(_) => {
-    //         return HttpResponse::InternalServerError().body("Failed to get environment variable");
-    //     }
-    // };
+    let save_dir = match std::env::var("SAVE_DIR") {
+        Ok(val) => val,
+        Err(_) => {
+            return HttpResponse::InternalServerError().body("Failed to get environment variable");
+        }
+    };
 
-    // let chrome_dir = match std::env::var("CHROME_DIR") {
-    //     Ok(val) => val,
-    //     Err(_) => {
-    //         return HttpResponse::InternalServerError().body("Failed to get environment variable");
-    //     }
-    // };
-    let chrome_parma = format!("--browser-executable-path {}", "/home/wuxin/Desktop/chrome-linux64/chrome");
-    let out_put_dir = format!("--output-directory {}", "/home/wuxin/Desktop/singleTest/");
+    let chrome_dir = match std::env::var("CHROME_DIR") {
+        Ok(val) => val,
+        Err(_) => {
+            return HttpResponse::InternalServerError().body("Failed to get environment variable");
+        }
+    };
+    // let chrome_parma = format!("--browser-executable-path {}", "/home/wuxin/Desktop/chrome-linux64/chrome");
+    // let out_put_dir = format!("--output-directory {}", "/home/wuxin/Desktop/singleTest/");
+
+    let chrome_parma = format!("--browser-executable-path {}",chrome_dir );
+    let out_put_dir = format!("--output-directory {}", save_dir);
 
 
     let url = format!("\"{}\"", params.str);
@@ -33,20 +36,47 @@ async fn read_later(params: web::Query<Params>) -> impl Responder {
     );
     println!("{}", cmd);
 
-    let output = std::process::Command::new("bash")
-        .arg("-c")
-        .arg(cmd)
-        .output()
-        .expect("Failed to execute command");
+    // cmd async exe
+    tokio::spawn(async move {
+        let output = Command::new("bash")
+            .arg("-c")
+            .arg(cmd)
+            .output()
+            .await;
+        match output {
+            Ok(output) => {
+                if output.status.success() {
+                    println!("Command executed successfully");
+                } else {
+                    println!("Command execution failed");
+                }
+            }
+            Err(e) => {
+                println!("Failed to execute command: {}", e);
+            }
+        }
+    });
 
-    if output.status.success() {
-        HttpResponse::Ok().body(format!(
-            "Page '{}' added successfully",
-            params.str
-        ))
-    } else {
-        HttpResponse::InternalServerError().body("Failed to execute command")
-    }
+    // http success
+    HttpResponse::Ok().body(format!(
+        "Page '{}' added successfully",
+        params.str
+    ))
+
+    // let output = std::process::Command::new("bash")
+    //     .arg("-c")
+    //     .arg(cmd)
+    //     .output()
+    //     .expect("Failed to execute command");
+
+    // if output.status.success() {
+    //     HttpResponse::Ok().body(format!(
+    //         "Page '{}' added successfully",
+    //         params.str
+    //     ))
+    // } else {
+    //     HttpResponse::InternalServerError().body("Failed to execute command")
+    // }
 }
 
 #[derive(Debug, Deserialize)]
@@ -60,7 +90,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(read_later)
     })
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
